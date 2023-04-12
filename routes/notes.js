7dc3e9034e50691234be63a7b0380291f994953a
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-04-12 15:11:21 -0300

    update

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,15 @@ const db = require('../db/connection')
 const {ObjectId} = require('mongodb')
 const router = Router()
 
+
+
+//view de detalhes na nota
+router.get('/:id', async function(req, res) {
+    const id = new ObjectId(req.params.id);
+    const note = await db.getDb().db().collection('notes').findOne({_id: id});
+    res.render('notes/detail', {note});
+});
+
 //form criação de rota
 router.get('/', function (req, res){
     res.render('notes/create')
@@ -17,6 +26,25 @@ router.post('/', function(req, res ){
     .insertOne({title: title, description: description})
 
     res.redirect( 301, '/')
+})
+
+//view de edicao de nota
+router.get('/edit/:id', async function(req, res){
+    const id = new ObjectId(req.params.id);
+    const note = await db.getDb().db().collection('notes').findOne({_id: id});
+    res.render('notes/edit', {note})
+} )
+
+//edicao de notas
+router.post('/update', function(req, res){
+    const data = req.body;
+    const id = new ObjectId( data.id);
+    const title = data.title;
+    const description = data.description;
+    console.log(title);
+    db.getDb().db().collection('notes').updateOne({_id: id}, {$set: {title: title, description: description} });
+    
+    res.redirect('/');
 })
 
 
